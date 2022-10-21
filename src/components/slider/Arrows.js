d0da0ba9abf5c7995ebdfd2dde9da0ab5fc7a7c1
Banky <bissbanky@gmail.com>
2022-10-21 09:56:26 +0100

    update

--- src/components/slider/Arrows.js
+++ src/components/slider/Arrows.js
@@ -1,31 +1,29 @@
 import React from "react";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import "./slider.css";
+import "./Arrows.css";
 
 function Arrows({ prevSlide, nextSlide, testimonial }) {
   return (
     <div className="arrows">
       <ArrowBackIosNewIcon
-        fontSize="large"
         className={`prev absolute ${testimonial ? "top-[35%]" : "top-[50%]"} ${
           testimonial ? "left-[-10px]" : "left-[0]"
         } cursor-pointer text-white ${
           testimonial ? "bg-transparent" : "bg-gray-500"
         } ${
-          testimonial ? "hover:bg-gray-500" : "hover:bg-gray-500"
-        } p-2 rounded-r `}
+          testimonial ? "hover:bg-gray-500" : "hover:bg-gray-700"
+        } p-[2px] bigTablet:p-1 rounded-r `}
         onClick={() => prevSlide()}
       />
       <ArrowForwardIosIcon
-        fontSize="large"
         className={`next absolute ${testimonial ? "top-[35%]" : "top-[50%]"} ${
           testimonial ? "right-[-10px]" : "right-[0]"
         } cursor-pointer  text-white ${
           testimonial ? "bg-transparent" : "bg-gray-500"
         } ${
-          testimonial ? "hover:bg-gray-500" : "hover:bg-gray-500"
-        } p-2 rounded-l`}
+          testimonial ? "hover:bg-gray-500" : "hover:bg-gray-700"
+        } p-[2px] bigTablet:p-1 rounded-l`}
         onClick={() => nextSlide()}
       />
     </div>
